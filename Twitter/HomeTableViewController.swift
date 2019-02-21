6130389528bd482675d2610ac96115237e677a5d
Mayur Barge <bargemayur05@gmail.com>
2019-02-21 00:30:00 -0800

    Logout functionality

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Mayur Barge on 2/21/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}

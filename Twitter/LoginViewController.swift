c8fdb9df70e64f10419b911ddb5d683d03ebefcc
Mayur Barge <bargemayur05@gmail.com>
2019-02-21 00:02:37 -0800

    Twitter authentication

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBAction func onLoginButton(_ sender: Any) {
-        print("Hello World")
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not log in!")
+        })
     }
     
     override func viewDidLoad() {

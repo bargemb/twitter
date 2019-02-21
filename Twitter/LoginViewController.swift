48f7782f3124d7f46d1e7ffa60f0545a8add3029
Mayur Barge <bargemayur05@gmail.com>
2019-02-20 23:54:45 -0800

    Login screen components

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Mayur Barge on 2/20/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func onLoginButton(_ sender: Any) {
+        print("Hello World")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

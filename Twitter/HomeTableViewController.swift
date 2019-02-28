a6c7ef88351719d069fe7691e45c80453247527c
Mayur Barge <bargemayur05@gmail.com>
2019-02-28 01:58:29 -0800

    Retweet functionality

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -81,6 +81,7 @@ class HomeTableViewController: UITableViewController {
         
         cell.setFavourite(tweetArray[indexPath.row]["favorited"] as! Bool)
         cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
 
